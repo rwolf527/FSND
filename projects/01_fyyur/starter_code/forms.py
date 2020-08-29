be281339f06efd72d6bd600bb51741c3f1fa5b38
rwolf <rwolf@cisco.com>
2020-08-29 12:46:26 -0400

    Ready for Final Review

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,220 +1,207 @@
 from datetime import datetime
 from flask_wtf import Form
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
-from wtforms.validators import DataRequired, AnyOf, URL
+from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
+from wtforms.validators import DataRequired, AnyOf, URL, Optional, ValidationError, Regexp
+from models import Artist, Venue
+
+# The following Regular Expression should allow for MOST valid phone numbers:
+# Note: Area code cannot start with a 0 or 1
+#       Prefix cannot start with a 0 or 1
+# see: https://stackoverflow.com/questions/123559/how-to-validate-phone-numbers-using-regex
+
+VALID_PHONE_NUMBER_REGEX = "^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$"
+
+VALID_GENRES = [
+    "Alternative",
+    "Blues",
+    "Classical",
+    "Country",
+    "Electronic",
+    "Folk",
+    "Funk",
+    "Hip-Hop",
+    "Heavy Metal",
+    "Instrumental",
+    "Jazz",
+    "Musical Theater",
+    "Pop",
+    "R&B",
+    "Reggae",
+    "Rock n Roll",
+    "Soul",
+    "Swing",
+    "Other",
+]
+
+VALID_STATES = [
+    "AL",
+    "AZ",
+    "AR",
+    "CA",
+    "AK",
+    "CO",
+    "CT",
+    "DE",
+    "DC",
+    "FL",
+    "GA",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "OH",
+    "OK",
+    "OR",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "PA",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY",
+]
+
+
+def multi_select_validator(valid_values):
+    message = "Invalid value, must be one of: {0}.format(", ".join(valid_values)"
+
+    def _validate(form, field):
+        for next_field_value in field.data:
+            if next_field_value not in valid_values:
+                raise ValidationError(message)
+
+    return _validate
+
+
+def artist_id_validator(form, field):
+    # First ensure that the incoming value is an integer
+    value = field.data
+    try:
+        int(value)
+    except Exception:
+        raise ValidationError("Artist Id must be an Integer")
+
+    num_found = Artist.query.filter_by(id=value).count()
+    if num_found == 0:
+        raise ValidationError(f"No Artists Found with Id: {value}")
+
+
+def venue_id_validator(form, field):
+    # First ensure that the incoming value is an integer
+    value = field.data
+    try:
+        int(value)
+    except Exception:
+        raise ValidationError("Venue Id must be an Integer")
+
+    num_found = Venue.query.filter_by(id=value).count()
+    if num_found == 0:
+        raise ValidationError(f"No Venues Found with Id: {value}")
+
 
 class ShowForm(Form):
-    artist_id = StringField(
-        'artist_id'
-    )
-    venue_id = StringField(
-        'venue_id'
-    )
-    start_time = DateTimeField(
-        'start_time',
-        validators=[DataRequired()],
-        default= datetime.today()
-    )
+    artist_id = StringField("artist_id", validators=[DataRequired(), artist_id_validator])
+    venue_id = StringField("venue_id", validators=[DataRequired(), venue_id_validator])
+    start_time = StringField("start_time", validators=[DataRequired()])
+
 
 class VenueForm(Form):
-    name = StringField(
-        'name', validators=[DataRequired()]
-    )
-    city = StringField(
-        'city', validators=[DataRequired()]
-    )
+    name = StringField("name", validators=[DataRequired()])
+    city = StringField("city", validators=[DataRequired()])
     state = SelectField(
-        'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        "state",
+        validators=[DataRequired(), AnyOf(VALID_STATES)],
+        choices=[(state, state) for state in VALID_STATES],
     )
-    address = StringField(
-        'address', validators=[DataRequired()]
-    )
+    address = StringField("address", validators=[DataRequired()])
     phone = StringField(
-        'phone'
+        "phone",
+        validators=[
+            Optional(),
+            Regexp(
+                VALID_PHONE_NUMBER_REGEX,
+                message="Please enter a valid phone number.",
+            ),
+        ],
     )
-    image_link = StringField(
-        'image_link'
-    )
+    image_link = StringField("image_link", validators=[Optional(), URL()])
+
     genres = SelectMultipleField(
-        # TODO implement enum restriction
-        'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
+        "genres",
+        validators=[DataRequired(), multi_select_validator(VALID_GENRES)],
+        choices=[(genre, genre) for genre in VALID_GENRES],
     )
     facebook_link = StringField(
-        'facebook_link', validators=[URL()]
+        # TODO implement enum restriction
+        "facebook_link",
+        validators=[Optional(), URL()],
     )
+
+    website = StringField("website", validators=[Optional(), URL()])
+
+    seeking_talent = BooleanField("seeking_talent")
+
+    seeking_description = StringField("seeking_description")
+
 
 class ArtistForm(Form):
-    name = StringField(
-        'name', validators=[DataRequired()]
-    )
-    city = StringField(
-        'city', validators=[DataRequired()]
-    )
+    name = StringField("name", validators=[DataRequired()])
+    city = StringField("city", validators=[DataRequired()])
     state = SelectField(
-        'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        "state",
+        validators=[DataRequired(), AnyOf(VALID_STATES)],
+        choices=[(state, state) for state in VALID_STATES],
     )
     phone = StringField(
-        # TODO implement validation logic for state
-        'phone'
+        "phone",
+        validators=[
+            Optional(),
+            Regexp(
+                VALID_PHONE_NUMBER_REGEX,
+                message="Please enter a valid phone number.",
+            ),
+        ],
     )
-    image_link = StringField(
-        'image_link'
-    )
+    image_link = StringField("image_link", validators=[Optional(), URL()])
+
     genres = SelectMultipleField(
-        # TODO implement enum restriction
-        'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
+        "genres",
+        validators=[DataRequired(), multi_select_validator(VALID_GENRES)],
+        choices=[(genre, genre) for genre in VALID_GENRES],
     )
     facebook_link = StringField(
-        # TODO implement enum restriction
-        'facebook_link', validators=[URL()]
+        "facebook_link",
+        validators=[Optional(), URL()],
     )
 
-# TODO IMPLEMENT NEW ARTIST FORM AND NEW SHOW FORM
+    website = StringField("website", validators=[Optional(), URL()])
+
+    seeking_venue = BooleanField("seeking_venue")
+
+    seeking_description = StringField("seeking_description")
---
+++ projects/01_fyyur/starter_code/models.py
@@ -0,0 +1,55 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Venue(db.Model):
+    __tablename__ = "Venue"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=False)
+
+    # TODO: COMPLETE implement any missing fields, as a database migration using Flask-Migrate
+    shows = db.relationship("Show", backref="venue", lazy="joined")
+
+    def __repr__(self):
+        return f"{self.name} in {self.city}, {self.state}"
+
+
+class Artist(db.Model):
+    __tablename__ = "Artist"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    shows = db.relationship("Show", backref="artist", lazy=True)
+
+    def __repr__(self):
+        return f"{self.name} from {self.city}, {self.state}"
+
+
+class Show(db.Model):
+    __tablename__ = "Show"
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime)
+    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"), nullable=False)
